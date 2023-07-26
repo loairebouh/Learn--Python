0296c0f399b4364de5ccf6514ac2c3ec9f976a36
LOAI rebouh <75275480+loairebouh@users.noreply.github.com>
2023-07-26 02:21:12 +0100

    1.0

--- Concatenationn.py
+++ Concatenationn.py
@@ -1,1 +1,25 @@
-# Im trying to push 
+# --------------------
+# -----Concatenation--
+# --------------------
+
+msg = "I love"
+lang = "Python"
+
+print(msg + " " + lang)
+
+full = msg + " " + lang
+
+print(full)
+
+a = "First \
+Second \
+Third"
+
+b = "A \
+B \
+C"
+
+print(a + "\n" + b)
+
+print("Hello\nWorld" + b)
+print("Hello\nWorld" + 1)  # ERROR
---
+++ Strings.py
@@ -0,0 +1,21 @@
+# -------------------
+# -----Strings-------
+# -------------------
+
+myStringOne = 'This Is Single Quote'
+myStringTwo = "This Is Second Quote"
+
+print(myStringOne)
+print(myStringTwo)
+
+myStringThree = 'This Is Single Quote " Test "'
+myStringFour = "This Is Second Quote 'Test' "
+
+print(myStringThree)
+print(myStringFour)
+
+myStringFive = """First
+Second
+Third"""
+
+print(myStringFive)
