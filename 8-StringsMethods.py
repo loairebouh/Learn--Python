fce44efe7345d5ecb29370a3ea07dd0ef1d3a147
LOAI rebouh <75275480+loairebouh@users.noreply.github.com>
2023-08-03 15:27:05 +0100

    1.1.3

---
+++ 8-StringsMethods.py
@@ -0,0 +1,9 @@
+# ------------------------
+# ----Strings Methods-----
+# ------------------------
+
+a = "I Love Python"
+print(len(a))  # Print The Lentgh Of The "a" String
+
+a = "I     Love       Python"
+print(len(b))  # Print The Lentgh Of The "b" String
