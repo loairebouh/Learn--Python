9f452d01a4f9d7760fe107f3e781de2472a81591
LOAI rebouh <75275480+loairebouh@users.noreply.github.com>
2023-08-03 15:02:25 +0100

    1.1.3

---
+++ 7-Strings-Indexing And Slicing.py
@@ -0,0 +1,19 @@
+# ---------------------------------
+# Strings Indexing & Slicing
+# [1] All Data in Python is Object
+# [2] Object Contain Elements
+# [3] Every Element Has Its Own Index
+# [4] Python Use Zero Based Indexing ( Index Start From Zero )
+# [5] Use Square Brackets To Access Element
+# [6] Enable Accessing Parts Of Strings, Tuples or Lists
+# ---------------------------------
+
+# Indexing ( Access Single Item )
+
+myString = "I Love Python"
+
+print(myString[0])  # Index (0) ==> I
+print(myString[9])  # Index (1) ==> t
+
+print(myString[0])  # Index (-1) ==> I
+print(myString[9])  # Index (1) ==> t
