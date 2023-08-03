ab18ad7d7a7c85a888573276c3aaa6872645c8c5
LOAI rebouh <75275480+loairebouh@users.noreply.github.com>
2023-08-03 15:04:36 +0100

    1.1.3

--- 7-Strings-Indexing And Slicing.py
+++ 7-Strings-Indexing And Slicing.py
@@ -17,3 +17,19 @@ print(myString[9])  # Index (1) ==> t
 
 print(myString[0])  # Index (-1) ==> I
 print(myString[9])  # Index (1) ==> t
+# Slicing ( Access Multiple Sequence Items )
+# [Start:End] End Not Included
+# [Start:End:Steps]
+
+print(myString[8:11])  # yth
+print(myString[3:5])  # ov
+
+print(myString[:10])  # If Start Is Not Here Will Start From 0 (I Love Pyt)
+print(myString[5:])  # If End Is Not Here Will Go To The End (e Python)
+print(myString[:])  # Full Data
+
+print(myString[0::1])  # Full Data
+print(myString[::1])  # Full Data
+
+print(myString[::2])
+print(myString[::3])
