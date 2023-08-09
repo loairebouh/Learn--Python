b67c65dfd443c073c9dab703b2cae55a6a5d30da
LOAI rebouh <75275480+loairebouh@users.noreply.github.com>
2023-08-09 01:17:51 +0100

    1.3.0

--- 2-Intermediate/2-Handling&BuiltInFunction/Part1.py
+++
@@ -1,8 +0,0 @@
-# -------------------
-# -- File Handling --
-# -------------------
-# "a" Append  Open File For Appending Values, Create File If Not Exists
-# "r" Read    [Default Value] Open File For Read and Give Error If File is Not Exists
-# "w" Write   Open File For Writing, Create File If Not Exists
-# "x" Create  Create File, Give Error If File Exists
-# --------------------------------------------------
