4b290dba6d069596140e30c73abbe0067a2b3900
LOAI rebouh <75275480+loairebouh@users.noreply.github.com>
2023-08-09 01:08:16 +0100

    1.3.2

---
+++ 2-Intermediate/1-Classes/Class.py
@@ -0,0 +1,12 @@
+class Employe:
+    def __init__(self, name, age, departement, is_manager):
+        self.name = name
+        self.age = age
+        self.departement = departement
+        self.is_manager = is_manager
+
+class Student : 
+    def __init__(self, name , classeroom , gpa)
+        self.name = name
+        self.classeroom = classeroom 
+        self.gpa = gpa
---
+++ 2-Intermediate/1-Classes/app.py
@@ -0,0 +1,15 @@
+from Class import Employe
+
+employe1 = Employe("loai", 19, "graphic designer", True)
+employe2 = Employe("Iyad", 17, "Trader", False)
+
+
+print(employe1.name)
+
+print(employe2.name)
+
+from Class import Student 
+
+Student1 = Student("loai" , "2nd year " , 8,20)
+
+Student2 = Student("Salah" , "2nd year " , 9,20)
